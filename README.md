0d9812908e1dfeaefcf18703aa7193f1e65d669c
stefanocudini <stefano.cudini@gmail.com>
2019-06-07 16:16:19 +0200

    refact files

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
+
 ![Image](images/test_osm_comune.png)
+
+il file:
+*trento_civici_con_nomi_strade.csv*
+**lat,lon,number,alfa,zipcode,street**
+
+# Uso
+
+```
+npm install
+```
+
+# Scripts
+
+*shp2csv.sh*
+converte shape file scaricati dal comune in un file CSV con due colonne:
+```name, geom```
+
+*join-civici-nomistrade.sh*
+associa il nome della strada ad ogni civico con il *codice strada* prendendolo dal file csv
+
+geom contiene la definizione della geometria in WKT:
+```LINESTRING([ [lat1,lon1],[lat2,lon2], ...])```
---
+++ join-civici-nomistrade.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#use csvkit via pip
+##sudo pip install csvkit
+
+csvjoin -c "strada,Codice_via" trento_civici.csv trento_strade.csv > join.csv
+csvcut -c "x,y,civico_num,civico_let,civico_alf,cap,Appellativo,Prenome,Denominazione" join.csv > cut.csv
+
+##quoteall
+csvformat -U 1 cut.csv > format.csv
+
+#rename column in pelias format
+#https://github.com/pelias/csv-importer#overview
+cat format.csv | csvsql --query "SELECT x AS lat,y AS lon,civico_num AS number,civico_alf AS alfa,cap AS zipcode, Appellativo||' '||Prenome||' '||Denominazione AS street FROM stdin" > out.csv
