8b72a28f39a087b44a13377dcc02b559b55b3e5d
stefanocudini <stefano.cudini@gmail.com>
2019-06-12 17:33:42 +0200

    import sqlite

--- download.sh
+++ download.sh
@@ -14,3 +14,5 @@ aria2c -i download.conf -d $TMP -l download.log -x 8 --auto-file-renaming=false 
 #extract compressed
 unzip -d "${TMP}TRENTO_CIVICI_SHP" "${TMP}TRENTO_CIVICI_SHP.zip"
 unzip -d "${TMP}TRENTO_STRADE_SHP" "${TMP}TRENTO_STRADE_SHP.zip"
+
+mv "${TMP}TRENTO_STRADE_NOMI.csv" "${TMP}trento_strade_nomi.csv"
--- prepare.sh
+++ prepare.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 #
-
 TMP="./tmp/"
 TMPDB="${TMP}tmp.sqlite"
 DATA="./data/"
+
+CSV_CIV="${TMP}trento_civici.csv"
+CSV_STRADE="${TMP}trento_strade.csv"
+CSV_NOMI="${TMP}trento_strade_nomi.csv"
 
 if [ -f $TMPDB ]; then
 	rm -f $TMPDB
 fi
 
 #civici
-CSV_CIV="${TMP}trento_civici_geom.csv"
 rm -f $CSV_CIV
 ogr2ogr -f "CSV" -lco GEOMETRY=AS_XY -s_srs EPSG:3044 -t_srs EPSG:4326 $CSV_CIV "${TMP}TRENTO_CIVICI_SHP/civici_web.shp"
 ##contains columns X,Y as lon,lat
@@ -20,7 +22,6 @@ csvformat -U 1 "$CSV_CIV.tmp" > $CSV_CIV
 rm -f "$CSV_CIV.tmp"
 
 #strade geometrie
-CSV_STRADE="${TMP}trento_strade_geom.csv"
 rm -f $CSV_STRADE
 ogr2ogr -f "CSV" -lco GEOMETRY=AS_WKT -s_srs EPSG:3044 -t_srs EPSG:4326 $CSV_STRADE "${TMP}TRENTO_STRADE_SHP/grafo_web.shp"
 ##contains column "WKT" as a WKT LineString
@@ -29,31 +30,28 @@ mv $CSV_STRADE "$CSV_STRADE.tmp"
 csvformat -U 1 "$CSV_STRADE.tmp" > $CSV_STRADE
 rm -f "$CSV_STRADE.tmp"
 
-
-#convert to utf8
+#strade nomi
 echo "convert to utf8..."
-mv "${TMP}TRENTO_STRADE_NOMI.csv" "${TMP}TRENTO_STRADE_NOMI.win.csv"
+mv $CSV_NOMI "${TMP}TRENTO_STRADE_NOMI.win.csv"
 iconv -f WINDOWS-1252 -t UTF-8//TRANSLIT "${TMP}TRENTO_STRADE_NOMI.win.csv" -o "${TMP}TRENTO_STRADE_NOMI.utf.csv"
-#quoteall
-csvformat -U 1 "${TMP}TRENTO_STRADE_NOMI.utf.csv" > "${TMP}TRENTO_STRADE_NOMI.csv"
+#quoteall, tab delimited char
+csvformat --tabs -U 1 "${TMP}TRENTO_STRADE_NOMI.utf.csv" > $CSV_NOMI
 
-
-
-#
 #import geometries
-#spatialite_tool -i -shp $INSHP -d $DB -c UTF-8 -t shp -s 4326 -g geom
+#spatialite_tool -i -shp $INSHP -d $TMPDB -c UTF-8 -t shp -s 4326 -g geom
 
-#import names
-#echo -e ".mode csv\n.separator ,\n.import $INCSV csv" | sqlite3 $DB
+echo -e ".mode csv\n.separator ,\n.import ${CSV_CIV} trento_civici" | sqlite3 $TMPDB
+echo -e ".mode csv\n.separator ,\n.import ${CSV_STRADE} trento_strade" | sqlite3 $TMPDB
+echo -e ".mode csv\n.separator ,\n.import ${CSV_NOMI} trento_strade_nomi" | sqlite3 $TMPDB
 
 #echo $2
 #TODO import nomi giusti da csv spatialite_tool -i -shp $1 -d $1.sqlite -t shape -s 4326 -g geom -c UTF-8
 
-#echo -e ".header on\n.mode csv\nSELECT (Appellativo||' '||Prenome||' '||Denominazione) AS name, AsWKT(geom) AS geom FROM shp,csv WHERE codice = CAST('Codice via' AS INTEGER);" | spatialite $DB > $OUT
+#echo -e ".header on\n.mode csv\nSELECT (Appellativo||' '||Prenome||' '||Denominazione) AS name, AsWKT(geom) AS geom FROM shp,csv WHERE codice = CAST('Codice via' AS INTEGER);" | spatialite $TMPDB > $OUT
 
 #spatialite_tool -e -d $1 -t shape -g geom -k -type LINESTRING
 
 
 #generate pelias polyline
-echo "generate polyline file..."
-node csv2polyline.js $CSV_STRADE > "${DATA}trento_strade_polyline.0sv"
+#echo "generate polyline file..."
+#node csv2polyline.js $CSV_STRADE > "${DATA}trento_strade_polyline.0sv"
