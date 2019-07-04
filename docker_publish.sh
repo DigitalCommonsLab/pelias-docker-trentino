e4c086310ff6229fe688e4b2f1fc315f3eaf9e90
stefanocudini <stefano.cudini@gmail.com>
2019-07-04 11:46:08 +0200

    dock pub script

--- docker_publish.sh
+++ docker_publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-TAG=$(git tag)
+TAG=$(git tag | tail -n1)
+TAG=${TAG//[$'\t\r\n']}
 
 docker build -t pelias-importer-trentino-opendata .
 docker tag pelias-importer-trentino-opendata stefcud/pelias-importer-trentino-opendata:$TAG
