def657a62cb45c699d416f88f49648f3f6f60c9b
stefanocudini <stefano.cudini@gmail.com>
2019-07-10 16:00:26 +0200

    format csv

--- docker_publish.sh
+++ docker_publish.sh
@@ -4,5 +4,9 @@ TAG=$(git tag | tail -n1)
 TAG=${TAG//[$'\t\r\n']}
 
 docker build -t pelias-importer-trentino-opendata .
+
+docker tag pelias-importer-trentino-opendata stefcud/pelias-importer-trentino-opendata:latest
+docker push stefcud/pelias-importer-trentino-opendata:latest
+
 docker tag pelias-importer-trentino-opendata stefcud/pelias-importer-trentino-opendata:$TAG
 docker push stefcud/pelias-importer-trentino-opendata:$TAG
